8323945de6222691d3f9c3f0df7206ac028f29ae
jebinpl <120675264+jebinpl@users.noreply.github.com>
2026-09-11 00:34:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,8 @@
-const CACHE_NAME = "holy-bible-v1";
+const CACHE_NAME = "holy-bible-v2";
 
 const APP_FILES = [
     "./",
     "./index.html",
-    "./style.css",
-    "./script.js",
-    "./manifest.json",
     "./logo.png"
 ];
 
@@ -49,7 +46,6 @@ const BIBLE_BOOKS = [
     "Haggai",
     "Zechariah",
     "Malachi",
-
     "Matthew",
     "Mark",
     "Luke",
@@ -88,59 +84,30 @@ const FILES_TO_CACHE = [
     ...BIBLE_FILES
 ];
 
-
-// INSTALL
 self.addEventListener("install", event => {
-
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(FILES_TO_CACHE);
-            })
+            .then(cache => cache.addAll(FILES_TO_CACHE))
             .then(() => self.skipWaiting())
     );
-
 });
 
-
-// ACTIVATE
 self.addEventListener("activate", event => {
-
     event.waitUntil(
-
         caches.keys().then(cacheNames => {
-
             return Promise.all(
-
                 cacheNames
                     .filter(name => name !== CACHE_NAME)
                     .map(name => caches.delete(name))
-
             );
-
         }).then(() => self.clients.claim())
-
     );
-
 });
 
-
-// FETCH
 self.addEventListener("fetch", event => {
-
     event.respondWith(
-
-        caches.match(event.request)
-            .then(cachedResponse => {
-
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-
-                return fetch(event.request);
-
-            })
-
+        caches.match(event.request).then(cachedResponse => {
+            return cachedResponse || fetch(event.request);
+        })
     );
-
 });
